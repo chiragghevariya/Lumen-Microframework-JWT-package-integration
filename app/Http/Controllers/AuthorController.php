b8c61369f4676f94774d7ebbd48eb1d75645a483
chirag <gcb1196@gmail.com>
2020-06-26 16:49:50 +0530

    project initialization done

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use Illuminate\Http\Request;
+use Validator;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthorController extends Controller
+{
+
+    public function showAllAuthors()
+    {
+        return response()->json(Author::all());
+    }
+
+    public function showOneAuthor($id)
+    {
+        return response()->json(Author::find($id));
+    }
+
+    public function create(Request $request)
+    {   
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:authors',
+            'location' => 'required|alpha'
+        ]);
+
+        $author = Author::create($request->all());
+
+        return response()->json($author, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $author = Author::findOrFail($id);
+        $author->update($request->all());
+
+        return response()->json($author, 200);
+    }
+
+    public function delete($id)
+    {
+        Author::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+
+    public function register(Request $request)
+    {
+        //validate incoming request 
+        $this->validate($request, [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+
+        try {
+           
+            $user = new User;
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            $plainPassword = $request->input('password');
+            $user->password = app('hash')->make($plainPassword);
+
+            $user->save();
+
+            //return successful response
+            return response()->json(['user' => $user, 'message' => 'CREATED'], 201);
+
+        } catch (\Exception $e) {
+            //return error message
+            return response()->json(['message' => 'User Registration Failed!'], 409);
+        }
+
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only(['email', 'password']);
+
+        if (! $token = Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+}
